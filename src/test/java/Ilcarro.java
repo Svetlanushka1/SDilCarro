a5997745ee90bbf90e61a9ab5c812852b399e580
Svetlanushka1 <durovasvetlanushka@gmail.com>
2022-11-17 11:23:21 +0200

    unsucess start
    gardle doesn't see Selenium Webdriver

---
+++ src/test/java/Ilcarro.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Ilcarro {
+    WebDriver 
+    @BeforeMethod
+    public void preCondition() {
+
+    }
+
+    @Test
+    public void search() {
+
+    }
+
+    @AfterMethod
+    public void postCondition() {
+
+    }
+}
